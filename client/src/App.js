65edf58aea8239339563d79aac488901f193277b
Radek Drlík <radek.drlik@gmail.com>
2023-07-18 23:40:16 +0200

    client react basic

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+function App() {
+    return ((0, jsx_runtime_1.jsx)("div", { className: "App", children: (0, jsx_runtime_1.jsx)("h1", { children: "Books" }) }));
+}
+exports.default = App;
---
+++ client/src/components/BookList.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+}
+
+const BookList: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3005/books')
+      .then(response => response.json())
+      .then(data => setBooks(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Book List</h1>
+      {books.map(book => (
+        <div key={book.id}>
+          <h2>{book.title}</h2>
+          <h3>{book.author}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
